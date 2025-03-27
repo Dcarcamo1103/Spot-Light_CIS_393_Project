5ea58bc967d458a507df2fd47a656b1378d0256b
Dcarcamo1103 <dcarcamo993@gmail.com>
2025-03-27 09:02:46 -0500

    minor changes

--- navbar.js
+++ navbar.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const navLinks = document.querySelectorAll('.nav_btn');
-    const currentPath = window.location.pathname;
+    const currentPath = window.location.pathname.split('/').pop(); // Get the last part of the URL path
 
     navLinks.forEach(link => {
         if (link.getAttribute('href') === currentPath) {
