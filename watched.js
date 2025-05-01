4cf0b069868b66849ffac9012008a174dc62d863
Dcarcamo <dcarcamo993@gmail.com>
2025-05-01 13:48:33 -0500

    minor visual and code changes

--- watched.js
+++ watched.js
@@ -4,7 +4,6 @@ document.getElementById('movieForm').addEventListener('submit', async function(e
     const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
     const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
-    // Remove genre fetching from the form
     const title = document.getElementById('title').value;
     const status = document.querySelector('input[name="status"]:checked').value;
 
@@ -153,8 +152,8 @@ function addMovieToTable(movie) {
 
     const actions = row.insertCell(5);
     actions.innerHTML = `
-        <button id="edit" class="list_btn" onclick="openEditModal(${JSON.stringify(movie)})">Edit <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#4B607F" id="edit_icon"><path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z"/></svg></button>
-        <button id="delete" class="list_btn" onclick="deleteMovie(this)">Delete <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#B73F1D" id="delete_icon"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg></button>
+        <button id="edit" class="list_btn" data-bs-toggle="modal" data-bs-target="#movieModal" onclick="openEditModal(${JSON.stringify(movie)})">Edit <i class="bi bi-pencil"></i></button>
+        <button id="delete" class="list_btn" onclick="deleteMovie(this)">Delete <i class="bi bi-trash3"></i></button>
     `;
 }
 
